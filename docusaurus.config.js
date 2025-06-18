4e4494aea411dc03ca75d2e8dfcb08a50e208f84
mohd arbaz khan <Arbaz1khan786@gmail.com>
2025-06-18 12:12:26 +0530

    chore: update site config and add logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Census Galaxy',
+  tagline: 'Documentation',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -79,12 +79,12 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/docusaurus-social-card.png',
       navbar: {
-        title: 'My Site',
+        title: 'Census Galaxy',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'Census Galaxy Logo',
+          src: 'img/censuslogo.jpg',
         },
         items: [
           {
